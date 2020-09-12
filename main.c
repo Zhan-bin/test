c6a9c7b21eb8d7c8d03876990072449f75e0c03e
Zhan-bin <807874967@qq.com>
2020-09-12 16:16:01 +0800

    test1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "HeapSort.h"
+#include "QuickSort.h"
+#include "BinarySearch.h"
+
+void test(int *array)
+{
+    printf("%d",*array);
+}
+
+int main() {
+    char *a = "1 2 3 ";
+    char b = '';
+    printf("%s", a);
+    return 0;
+}
